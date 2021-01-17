e28107189a9295f879c71462b7b453bbe4203cb4
yamadanagamasa <danieru565612345@gmail.com>
2021-01-17 16:17:21 +0900

    ddd

--- index.php
+++ index.php
@@ -25,8 +25,12 @@ fclose($fp);
 
 ?>
 <!DOCTYPE html>
+<head>
 <meta charset="UTF-8">
-<title>掲示板</title>
+<title>ぬぬ掲示板</title>
+<link rel="stylesheet" href="keiji.css">
+</head>
+<body>
 <h1>掲示板</h1>
 <section>
     <h2>新規投稿</h2>
@@ -49,3 +53,4 @@ fclose($fp);
     <p>投稿はまだありません</p>
 <?php endif; ?>
 </section>
+</body>
